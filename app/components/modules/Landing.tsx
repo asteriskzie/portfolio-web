b479e1982c883d0807dd4d3bb72c26384b3d7d7d
Ester Gracia <ester.gracia.10@gmail.com>
2023-08-12 18:45:57 +0700

    Update landing bg color

--- app/components/modules/Landing.tsx
+++ app/components/modules/Landing.tsx
@@ -1,7 +1,7 @@
 import { Button } from "../elements/Button";
 export const Landing = () => {
     return (
-        <div className="min-h-screen w-full grid place-content-center bg-gradient-radial from-federal_blue-300 via-black_raisin-200 to-black_raisin">
+        <div className="min-h-screen w-full grid place-content-center bg-gradient-radial from-federal_blue-400 via-raisin_black-200 to-raisin_black">
             <div className="flex flex-col text-center content-center space-y-6">
                 <p className="">Hello, my name is</p>
                 <h1 className="bg-gradient-to-r from-federal_blue-800 via-pink_lavender-400 to-dutch_white-500 bg-clip-text text-transparent text-8xl font-bold">Ester Gracia</h1>
