1374b0bd2fd2563cdd06772d9e0e395241c5c5d9
Ester Gracia <ester.gracia.10@gmail.com>
2023-08-12 18:28:30 +0700

    Style button

--- app/components/elements/Button.tsx
+++ app/components/elements/Button.tsx
@@ -1,7 +1,20 @@
-export const Button = () => {
+import React, { ReactNode } from 'react'
+
+interface ButtonProps {
+    children: ReactNode; 
+    filled?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({children, filled=true, ...attributes}) => {
+    let buttonClassName="cursor-pointer w-fit px-6 py-3 flex place-content-center rounded-xl "
+    if (filled) {
+        buttonClassName+="bg-white text-raisin_black font-bold"
+    } else {
+        buttonClassName+="text-white border-2 border-white "
+    }
     return (
-        <div className="cursor-pointer w-fit px-6 py-3 flex place-content-center bg-red-900 rounded-xl bg-white">
-            <button className="text-black ">clickable</button>
+        <div className={buttonClassName}>
+            <button>{children}</button>
         </div>
     )
 }
