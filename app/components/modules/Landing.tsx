327c0331a702a52c5171bf74007e71ff2ad39f84
Ester Gracia <ester.gracia.10@gmail.com>
2023-08-12 23:23:51 +0700

    Fix linking

--- app/components/modules/Landing.tsx
+++ app/components/modules/Landing.tsx
@@ -6,8 +6,8 @@ export const Landing = () => {
                 <p className="">Hello, my name is</p>
                 <h1 className="bg-gradient-to-r from-federal_blue-800 via-pink_lavender-400 to-dutch_white-500 bg-clip-text text-transparent text-8xl font-bold">Ester Gracia</h1>
                 <div className="flex flex-row space-x-8 place-content-center">
-                    <Button>Contact</Button>
-                    <Button filled={false}>Resume</Button>
+                    <Button href='#contacts'>Contact</Button>
+                    <Button filled={false} href='/resume.pdf' target='blank'>Resume</Button>
                 </div>
             </div>
         </div>
