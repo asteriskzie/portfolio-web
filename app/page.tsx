5ed54083b9e3d2741db7e96ee9bd27657d11dd66
Ester Gracia <ester.gracia.10@gmail.com>
2023-08-12 18:41:57 +0700

    Create landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Landing } from './components/modules/Landing'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-raisin-black">
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between bg-raisin_black">
       <Landing />
       <About />
     </main>
