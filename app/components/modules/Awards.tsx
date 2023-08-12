355d6c1c6b0ae8c26950b86102c8975b846e6fe8
Ester Gracia <ester.gracia.10@gmail.com>
2023-08-12 20:44:34 +0700

    Fix unescaped single quote characters (')

--- app/components/modules/Awards.tsx
+++ app/components/modules/Awards.tsx
@@ -1,9 +1,9 @@
 export const Awards = () => {
     return (
         <div className="min-h-screen w-full p-48 flex-col space-y-24">
-            <p>One day, I fell in love with programming. Competitive programming has been my way of expressing it ever since.</p>
+            <p className="text-center">One day, I fell in love with programming. Competitive programming has been my way of expressing it ever since.</p>
             <div>
-                <p> Testing out this stuff, I'll make a list of CP Achievements</p>
+                <p> Testing out this stuff, I will make a list of CP Achievements</p>
                 <ul>
                     <li>- Hehe</li>
                     <li>- Hehe</li>
